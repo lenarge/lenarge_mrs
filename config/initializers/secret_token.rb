23a5e193e7b89710273b84e9ae71ae5ac2c725cc
Neilor Caldeira <neilorcaldeira@gmail.com>
2014-03-21 14:14:04 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LenargeMrs::Application.config.secret_key_base = '7488af3b7a0daca40ea5b730b5b794d129f37d04a06df5030ad67e959a298f4f298ab29d882daafe5a84055595240e0577e048f6577e07317fa846fb5ec458a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LenargeMrs::Application.config.session_store :cookie_store, key: '_lenarge_mrs_session'
