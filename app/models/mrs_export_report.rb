1ce9292bfaaf4ec57588f7858d67102c9e844d98
Neilor Caldeira <neilorcaldeira@gmail.com>
2014-03-25 14:47:04 -0300

    fix txt report end line

--- app/models/mrs_export_report.rb
+++ app/models/mrs_export_report.rb
@@ -18,7 +18,7 @@ class MrsExportReport
       txt << sprintf("%015.2f", rodo_cte.amount).sub('.', ',') # Valor do Cte
       txt << sprintf("%013.2f", 0).sub('.', ',') # Iss do Cte
       txt << sprintf("%010.2f", 0).sub('.', ',') # Icms do Cte
-      txt << "\n"
+      txt << "\r\n"
 
       rodo_cte.cte_allocations.each do |allocation|
         txt << '20'
@@ -31,7 +31,7 @@ class MrsExportReport
         txt << sprintf("%06d", allocation.ferro_cte.number) # Número do Ferro Cte
         txt << '25011'
         txt << sprintf("%010.2f", (allocation.weight/1000).round(2)).sub('.', ',') # Peso alocado
-        txt << "\n"
+        txt << "\r\n"
       end
     end
 
