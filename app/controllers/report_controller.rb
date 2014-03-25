d9771e023a7bbae8a938b7b5a72c0909e50fb9af
Neilor Caldeira <neilorcaldeira@gmail.com>
2014-03-25 14:19:01 -0300

    fix date params on report controller

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -4,7 +4,7 @@ class ReportController < ApplicationController
 
   def mrs_export_extract
     start_date = Date.new(params[:start_date][:year].to_i, params[:start_date][:month].to_i, params[:start_date][:day].to_i)
-    end_date = Date.new(params[:end_date][:year].to_i, params[:end_date][:month].to_i, params[:end_date][:day].to_i)
+    end_date = Date.new(params[:end_date][:year].to_i, params[:end_date][:month].to_i, params[:end_date][:day].to_i) + 1.day
 
     @content = MrsExportReport.new(start_date: start_date, end_date: end_date).to_text
 
